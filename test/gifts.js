349f6d48207ce5d0bf2ec502bf26b04c074edb35
atlfinesse <finesse.cap@gmail.com>
2020-11-16 09:43:13 -0800

    tests set up

---
+++ test/gifts.js
@@ -0,0 +1,10 @@
+const should = require('chai').should()
+const expect = require('chai').expect
+const supertest = require('supertest')
+
+// Pass supertest the API endpoint that we will be using
+const api = supertest('http://localhost:4000')
+
+describe("GET /gifts", () => {
+  
+})
