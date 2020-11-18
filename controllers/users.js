d31b4f84ac260a5aaca5546295e425cd4244697a
atlfinesse <finesse.cap@gmail.com>
2020-11-17 19:07:23 -0800

    Set up user-auth

---
+++ controllers/users.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt')
+
+
+// SIGN UP
+// POST /user/signup
+router.post('/signup', (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then(hash =>
+      ({
+        email: req.body.email,
+        password: hash
+      })
+    )
+    .then(user => User.create(user))
+    .then(user => res.status(201).json(user))
+    .catch(next);
+});
+
+
+// SIGN IN
+// POST /user/signin
+router.post('/signin', (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    // .then((user) => createUserToken(req, user))
+    // .then((token) => res.json({ token }))
+    .catch(next);
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
